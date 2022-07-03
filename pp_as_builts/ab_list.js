a7d8c59c6f8e78c008d4e9a0b547903c49f8a33d
chris <christopher.logston@sfdpw.org>
2022-07-02 19:21:41 -0700

    modified:   data/1164I_010_PRJCT_DWGS_64.js 	new file:   pp_as_builts/1164I_R-02_AB_PP03.pdf 	new file:   pp_as_builts/1164I_R-03_AB_PP03.pdf 	new file:   pp_as_builts/1164I_R-07_AB_PP03.pdf 	new file:   pp_as_builts/1164I_SW-09_AB_PP03.pdf 	new file:   pp_as_builts/1164I_SW-11_AB_PP03.pdf 	modified:   pp_as_builts/ab_list.js

--- pp_as_builts/ab_list.js
+++ pp_as_builts/ab_list.js
@@ -4,11 +4,17 @@ var json_ab_list = {
 
 '1164I_SW-07_AB_PP01',
 '1164I_SW-07_AB_PP02',
-'1164I_SW-08_AB_PP02'
+'1164I_SW-08_AB_PP02',
+'1164I_SW-09_AB_PP03',
+'1164I_SW-11_AB_PP03'
 
 ],
  
 "R": [
+
+'1164I_R-02_AB_PP03',
+'1164I_R-03_AB_PP03',
+'1164I_R-07_AB_PP03'
 
 ], 
  
