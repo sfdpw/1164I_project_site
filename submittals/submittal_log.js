bd97a0313ad87f5ba32e0ea1ca593c5e13697665
chris <christopher.logston@sfdpw.org>
2022-12-01 16:03:33 -0800

    modified:   data/1164I_101_sewer_lines_0.js 	modified:   submittals/submittal_log.js

--- submittals/submittal_log.js
+++ submittals/submittal_log.js
@@ -7,6 +7,21 @@ var json_submittals = {
  'response': {'general': {'desc':'-','resp':'-' }, 'item_01': {'desc':'-','resp':'-' }, 'item_02': {'desc': '-'} },
 }, 
 
+
+'74': {
+ 'description':'-',
+ 'type':'-',
+ 'dates': { 'cnt_to_cm': '-', 'cm_to_dsn': '-', 'dsn_to_cm': '-', 'cm_to_cnt': '4/22/2022' },
+ 'response': {'general': {'desc':'-','resp':'-' }, 'item_01': {'desc':'-','resp':'-' }, 'item_02': {'desc': '-'} },
+}, 
+
+'75': {
+ 'description':'-',
+ 'type':'-',
+ 'dates': { 'cnt_to_cm': '-', 'cm_to_dsn': '-', 'dsn_to_cm': '-', 'cm_to_cnt': '4/25/2022' },
+ 'response': {'general': {'desc':'-','resp':'-' }, 'item_01': {'desc':'-','resp':'-' }, 'item_02': {'desc': '-'} },
+}, 
+
 '92': {
  'description':'-',
  'type':'-',
