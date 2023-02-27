c23260178741596ec8e6f51eeba3e7f0154a4d4e
chris <christopher.logston@sfdpw.org>
2023-02-27 12:15:36 -0800

    modified:   data/1164I_101_sewer_lines_0.js 	modified:   submittals/submittal_log.js

--- submittals/submittal_log.js
+++ submittals/submittal_log.js
@@ -309,6 +309,13 @@ var json_submittals = {
  'response': {'general': {'desc':'-','resp':'-' }, 'item_01': {'desc':'-','resp':'-' }, 'item_02': {'desc': '-'} },
 },
 
+'167.1': {
+ 'description':'-',
+ 'type':'-',
+ 'dates': { 'cnt_to_cm': '-', 'cm_to_dsn': '-', 'dsn_to_cm': '-', 'cm_to_cnt': '2/27/2023' },
+ 'response': {'general': {'desc':'-','resp':'-' }, 'item_01': {'desc':'-','resp':'-' }, 'item_02': {'desc': '-'} },
+},
+
 '168': {
  'description':'-',
  'type':'-',
